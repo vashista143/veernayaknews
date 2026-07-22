c1c44a5f163ffd36765e9325aff802ba58b8b609
vashista <vashista.dara03@gmail.com>
2026-07-22 12:48:09 +0530

    Update news.route.js

--- routes/news.route.js
+++ routes/news.route.js
@@ -12,6 +12,9 @@ router.get("/breaking", newsController.getBreakingNews);
 router.get("/category/:category", newsController.getNewsByCategory);
 router.get("/related/:id", newsController.getRelatedNews);
 
+router.get("/my-submissions", newsController.getMySubmissions);
+
+
 // Global Document Collections
 router.get("/", newsController.getAllNews);
 router.get("/:id", newsController.getNewsById);
